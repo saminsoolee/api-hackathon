12231511f352b6a818b604eebee1f456deb59734
saminsoolee <samuelminsoolee71@gmail.com>
2020-08-19 16:14:59 +0000

    User can see random quote - Sam Lee

--- feature-1/main.js
+++ feature-1/main.js
@@ -1,11 +1,12 @@
 var key = "71e9a5e20d69c0d631300705fc66962f6155ff6f5007ec4d356ba293afc3c3d1"
-var quoteContainer = document.querySelector(".quote-container > h5");
+var quoteContainer = document.querySelector(".quote-container > p");
 
 $.ajax({
   type: "GET",
   url: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD,ETH,LTC&api_key=" + key,
   success: data => console.log(data)
 })
+
 $.ajax({
   type: "GET",
   url: "https://quote-garden.herokuapp.com/api/v2/quotes/random",
